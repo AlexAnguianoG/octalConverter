132be6180a874f9ab03581bd6bd181cb0dc0594c
Alejandro Aguiano <alex1203@live.com.mx>
2021-04-05 20:30:20 -0600

    Challenge 3

--- OctalConverter.js
+++ OctalConverter.js
@@ -1,5 +1,9 @@
-function convert(a) {
-  return a;
+function convert(dec) {
+  var a = Math.floor(dec / 8);
+  var b = dec % 8;
+  var c = a * 10 + b;
+
+  return c;
 }
 
 exports.convert = convert;
